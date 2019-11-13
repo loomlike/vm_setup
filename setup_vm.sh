dcc16053bdbef2628277f71765ebcb7b09613cef
JS <jiata@microsoft.com>
2019-11-13 16:52:48 -0500

    32 users

--- setup_vm.sh
+++ setup_vm.sh
@@ -11,10 +11,10 @@ conda activate cv
 python -m ipykernel install --name cv --display-name "MLADS CV LAB" 
 
 # add the 5 users to jupyterhub
-echo 'c.Authenticator.whitelist = {"mlads1", "mlads2", "mlads3", "mlads4", "mlads5"}' | sudo tee -a /etc/jupyterhub/jupyterhub_config.py
+echo 'c.Authenticator.whitelist = {"mlads1", "mlads2", "mlads3", "mlads4", "mlads5", "mlads6", "mlads7", "mlads8", "mlads9", "mlads10", "mlads11", "mlads12", "mlads13", "mlads14", "mlads15", "mlads16", "mlads17", "mlads18", "mlads19", "mlads20", "mlads21", "mlads22", "mlads23", "mlads24", "mlads25", "mlads26", "mlads27", "mlads28", "mlads29", "mlads30", "mlads31", "mlads32"}' | sudo tee -a /etc/jupyterhub/jupyterhub_config.py
 
 # create the users to the vm 
-for i in {1..5}
+for i in {1..32}
 do
     USERNAME=mlads$i
     PASSWORD=cvmlads$i
